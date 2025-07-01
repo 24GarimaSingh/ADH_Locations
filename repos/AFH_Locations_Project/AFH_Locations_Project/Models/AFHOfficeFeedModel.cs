645d8975ebf7a7de2d29294e62b1ebbe78c0ead3
Garima Singh <garima24jan@gmail.com>
2025-07-01 14:41:05 +0100

    Added fallback json file

--- repos/AFH_Locations_Project/AFH_Locations_Project/Models/AFHOfficeFeedModel.cs
+++ repos/AFH_Locations_Project/AFH_Locations_Project/Models/AFHOfficeFeedModel.cs
@@ -1,12 +1,22 @@
-﻿namespace AFH_Locations_Project.Models
+﻿using System.Text.Json.Serialization;
+
+namespace AFH_Locations_Project.Models
 {
     public class AFHOfficeFeedModel
     {
-        
-            public required string Name { get; set; }
-            public required string Address1 { get; set; }
-            public string? Address2 { get; set; }
-            public required string City { get; set; }
-            public required string PostCode { get; set; }
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
+
+        [JsonPropertyName("address1")]
+        public required string Address1 { get; set; }
+
+        [JsonPropertyName("address2")]
+        public string? Address2 { get; set; }
+
+        [JsonPropertyName("city")]
+        public required string City { get; set; }
+
+        [JsonPropertyName("postcode")]
+        public required string PostCode { get; set; }
     }
 }
