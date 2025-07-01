880b51a50e9190d24649a2c59beff35dea4fe28a
Garima Singh <garima24jan@gmail.com>
2025-07-01 13:24:54 +0100

    AFH Project commit

---
+++ repos/AFH_Locations_Project/AFH_Locations_Project/Models/AFHOfficeFeedModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AFH_Locations_Project.Models
+{
+    public class AFHOfficeFeedModel
+    {
+        
+            public required string Name { get; set; }
+            public required string Address1 { get; set; }
+            public string? Address2 { get; set; }
+            public required string City { get; set; }
+            public required string PostCode { get; set; }
+    }
+}
---
+++ repos/AFH_Locations_Project/AFH_Locations_Project/Service/LocationService.cs
@@ -0,0 +1,34 @@
+﻿using AFH_Locations_Project.Models;
+using Microsoft.Extensions.Caching.Memory;
+namespace AFHOfficeFeedApi.Services;
+
+public class LocationService
+{
+    private readonly IMemoryCache _cache;
+    private readonly HttpClient _httpClient;
+    private const string ApiUrl = "https://www.afhwm.co.uk/api/v1/locations";
+
+    public LocationService(IMemoryCache cache, HttpClient httpClient)
+    {
+        _cache = cache;
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<AFHOfficeFeedModel>> GetLocationsAsync()
+    {
+        const string cacheKey = "office_locations";
+
+        if (_cache.TryGetValue(cacheKey, out List<AFHOfficeFeedModel> cachedLocations))
+        {
+            return cachedLocations;
+        }
+
+        var result = await _httpClient.GetFromJsonAsync<List<AFHOfficeFeedModel>>(ApiUrl);
+        var locations = result?.Take(12).ToList() ?? new List<AFHOfficeFeedModel>();
+
+       _cache.Set(cacheKey, locations, TimeSpan.FromMinutes(1));
+
+        return locations;
+    
+    }
+}
